2d064fd5c445bf3c82eb26c54368c8352dc5784e
Kaick Marinho <kaickmarinho123@gmail.com>
2025-09-03 22:43:54 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,13 +1,18 @@
 package application;
 
+import java.time.LocalDate;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
 	public static void main(String[] args) {
 		
 		Department obj = new Department(1, "books");
-		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "bob" , "sla" , LocalDate.now(), 2000.0, obj);
+		System.out.println(seller);
 	}
 
 }
